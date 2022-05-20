21cf74f6d1b446d8ba51a33721a95ccc964a29a0
Alexa Cristina <alexa@maptheclouds.com>
2022-05-20 11:23:41 +0300

    feat: pause on unfocus

--- polling/exercise/frontend/polling-chat.js
+++ polling/exercise/frontend/polling-chat.js
@@ -50,7 +50,6 @@ async function getNewMsgs() {
 
   allChat = json.msg;
   render();
-  setTimeout(getNewMsgs, INTERVAL);
 }
 
 function render() {
@@ -66,5 +65,17 @@ function render() {
 const template = (user, msg) =>
   `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
 
-// make the first request
-getNewMsgs();
+let timeToMakeNextRequest = 0;
+async function rafTimer(time) {
+  // console.log(time);
+
+  if (timeToMakeNextRequest <= time) {
+    await getNewMsgs();
+
+    timeToMakeNextRequest = time + INTERVAL;
+  }
+
+  requestAnimationFrame(rafTimer);
+}
+
+requestAnimationFrame(rafTimer);
